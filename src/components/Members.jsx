8c2666c5fa4af2d13784121ce468c2777f6a2e7c
Navi12-18 <91783788+Navi12-18@users.noreply.github.com>
2022-11-11 17:52:29 +0530

    Update Members.jsx

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -10,28 +10,28 @@ const Members = () => {
     <p className="intro">We are a team of 4 from multi-displinary domain.</p>
 
     <div class="Member V">
-        <img src={require("./images/Harsh1.png")} alt="" srcset="" class="Harsh" />
+        <img src={require("./images/Vedant.png")} alt="" srcset="" class="Harsh" />
       <p class="name">Vedant Kutemate</p>
       <p class="work">UI/UX Designer</p>
       <a href="https://www.linkedin.com/in/vedant-kutemate-4336601b2/"><img class="image" src={require("./images/icons8-linkedin-32.png")} alt="linkedin icon"/></a>
     </div>
 
     <div class="Member N">
-      <img class="Harsh" src={require("./images/Harsh1.png")} alt="Harsh image"/>
+      <img class="Harsh" src={require("./images/Naivaidya.png")} alt="Harsh image"/>
       <p class="name">Naivaidya Yadav</p>
       <p class="work">Web Developer</p>
       <a href="https://www.linkedin.com/in/naivaidya-yadav-0179b7200/"><img class="image" src={require("./images/icons8-linkedin-32.png")} alt="linkedin icon"/></a>
     </div>
 
     <div class="Member H">
-      <img class="Harsh" src={require("./images/Harsh1.png")} alt="Harsh image"/>
+      <img class="Harsh" src={require("./images/Harsh.png")} alt="Harsh image"/>
       <p class="name">Harsh Vardhan Singh</p>
       <p class="work">Data Analyst</p>
       <a href="https://linkedin.com/in/harsh-vardhan-singh-9a1563219"><img class="image" src={require("./images/icons8-linkedin-32.png")} alt="linkedin icon"/></a>
     </div>
 
     <div class="Member S">
-      <img class="Harsh" src={require("./images/Harsh1.png")} alt="Harsh image"/>
+      <img class="Harsh" src={require("./images/Shashank.png")} alt="Harsh image"/>
       <p class="name">Shashank Sanket</p>
       <p class="work">Python Developer</p>
       <a href="https://www.linkedin.com/in/shashank-sanket-89b30b209/"><img class="image" src={require("./images/icons8-linkedin-32.png")} alt="linkedin icon"/></a>
